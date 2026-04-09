0471200dd0789cf834a76cbdbb58083e051f6bb5
Mahathi1601 <mahasabbani16@gmail.com>
2026-04-09 23:06:04 +0530

    Added config.js file

---
+++ frontend/config.js
@@ -0,0 +1,7 @@
+// config.js
+const API_BASE_URL = 'https://artify-2iq3.onrender.com/api';
+
+// Export for use in other files
+if (typeof module !== 'undefined' && module.exports) {
+  module.exports = { API_BASE_URL };
+}
